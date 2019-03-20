a8a03372ab87c899049e0359b0c11a6ef4e78828
smeerws <hello@smeerws.at>
2019-03-20 17:16:55 +0100

    init scenemanagement button load all scenes debug in console

---
+++ Assets/MyGame/Scripts/LoadScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public void LoadGame1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ListAllScenes()
+    {
+        List<string> scenes = new List<string>();
+
+        foreach (var sceneEntry in EditorBuildSettings.scenes)
+        {
+            Debug.Log(sceneEntry.path);
+        }
+                   
+    }
+
+    
+}
